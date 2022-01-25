441b32c2ef2f8cc80ea84fc15296070ef3a44e0c
ErikRodrigues <erickviips16@gmail.com>
2022-01-24 21:16:44 -0300

    Criando class SellerDao e Factory

--- src/model/dao/DerpatmentDao.java
+++ src/model/dao/DerpatmentDao.java
@@ -1,5 +1,15 @@
-package model.dao;
+ package model.dao;
+
+import java.util.List;
+
+import model.entities.Department;
 
 public interface DerpatmentDao {
+	
+	void insert(Department obj);
+	void update(Department obj);
+	void deleteById(Integer Id);
+	Department findById(Integer id);
+	List<Department> findAll();
 
 }
