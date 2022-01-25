7eba4aff21bbf836823f652febe69b519bca68e9
ErikRodrigues <erickviips16@gmail.com>
2022-01-25 16:04:27 -0300

    Resuando a instanciação

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,6 +11,8 @@ public class Program {
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
+		
+		System.out.println("=== TEST 1: Vamos tester o findbyid do Seller");
 		Seller seller = sellerDao.findById(3);
 		
 		System.out.println(seller);
